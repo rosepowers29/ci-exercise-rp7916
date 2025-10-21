97bc7a81cd64fb337380a1b68328cb1779f5467c
Rose Powers <rosepowers@dynamic-oit-ip4-wifirestricted04-10-50-120-233.princeton.edu>
2025-10-20 21:03:57 -0400

    test github workflows

---
+++ docs/conf.py
@@ -0,0 +1,4 @@
+project = "unc"
+extensions = ["sphinx.ext.autodoc", "myst_parser", "sphinx-markdown-builder"]
+source_suffix = [".rst", ".md"]
+html_theme = "furo"
---
+++ noxfile.py
@@ -0,0 +1,22 @@
+import nox
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    """
+    Run the unit and regular tests.
+    """
+    test_deps = nox.project.dependency_groups(PROJECT, "test")
+    session.install("-e.", *test_deps)
+    session.run("pytest", *session.posargs)
+
+@nox.session
+def docs(session: nox.Session) -> None:
+    """
+    Build the docs. Pass "--serve" to serve.
+    """
+
+    session.install(".[docs]")
+    session.chdir("docs")
+    session.run("sphinx-build", "-M", "html", ".", "build")
+
